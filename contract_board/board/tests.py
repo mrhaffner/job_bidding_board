02ea4b5ba0a9471cf01b91ecd95c55441d74c20f
mrhaffner <mrhaffner13@gmail.com>
2023-02-15 23:00:11 -0600

    configured tests for linux

--- contract_board/board/tests.py
+++ contract_board/board/tests.py
@@ -1,3 +1,5 @@
+import platform
+
 from datetime import datetime
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -42,12 +44,16 @@ class FunctionalTest(StaticLiveServerTestCase):
     """Tests the basic functionality of the bidding board."""
 
     def setUp(self):
-        service = ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-        options = Options()
-        options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
-        options.add_argument("--disable-gpu")
-        self.browser = webdriver.Chrome(service=service, options=options)
+        current_os = platform.platform()
+        if 'linux' in current_os:
+            service = ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+            options = Options()
+            options.add_argument("--no-sandbox")
+            options.add_argument("--headless")
+            options.add_argument("--disable-gpu")
+            self.browser = webdriver.Chrome(service=service, options=options)
+        else:
+            self.browser = webdriver.Chrome()
         self.base_url = 'http://127.0.0.1:8000/'
 
     def tearDown(self):
