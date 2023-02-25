d7f702cae2bd8089bf2de29584c211b1fbe1500b
Mahmoud Salem <mahmoud.s.salem.2000@gmail.com>
2023-02-24 20:24:35 -0600

    I think I got the logic right

--- contract_board/board/tests/test_authorization.py
+++ contract_board/board/tests/test_authorization.py
@@ -15,6 +15,15 @@ class AuthorizationPagesTest(FunctionalTest):
 
     def test_authenticated_redirected_from_login(self) -> None:
         """Tests that an authenticated user is redirected away from login page."""
+        self.browser.get(self.base_url)
+        self.browser.set_window_size(1024,768)
+        loged_in =self.browser.find_element(By.ID, "login") #if there is a login botton, 
+        #then it still needs to log in
+        self.assertEqual(len(loged_in),0)
 
     def test_authenticated_redirected_from_registration(self) -> None:
         """Tests that an authenticated user is redirected away from registration page."""
+        self.browser.get(self.base_url)
+        self.browser.set_window_size(1024,768)
+        registered =self.browser.find_element(By.ID, "Register")
+        self.assertEqual(len(registered),0) 
