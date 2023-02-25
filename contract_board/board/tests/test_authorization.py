2ff4b3488b353b3ee898d302bd31aa2592d4b647
Faiza <faizamohamoud826725@gmail.com>
2023-02-25 17:55:07 -0600

    Update test_authorization.py

--- contract_board/board/tests/test_authorization.py
+++ contract_board/board/tests/test_authorization.py
@@ -10,8 +10,15 @@ class AuthorizationPagesTest(FunctionalTest):
     def test_registration(self) -> None:
         """Tests user registration."""
 
-    def test_logout(self) -> None:
-        """Tests loging out a user."""
+   def test_logout(self) -> None:
+    # Login the user first
+    login_result = login(username="testuser", password="testpass")
+    self.assertEqual(login_result, "Login successful")
+
+    # Logout the user and check if the user is actually logged out
+    logout_result = logout()
+    self.assertEqual(logout_result, "Logout successful")
+    self.assertFalse(is_logged_in())
 
     def test_authenticated_redirected_from_login(self) -> None:
         """Tests that an authenticated user is redirected away from login page."""
